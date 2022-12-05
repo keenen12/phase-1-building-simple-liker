aa3d77c28bf6c36489812c010e75c08e6d430e84
Keenen Harral <kharral23@yahoo.com>
2022-12-04 17:24:43 -0800

    commit tree 86e38b570baf6e31832fdd1b4c115d1781f9a69c

--- main.js
+++ main.js
@@ -1,9 +1,51 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector('#modal')
+
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("dom has been loaded")
+  const errorModal = document.querySelector('#modal')
 
+  // FIND LIKES
+  clickListener();
+
+})
+
+function hideError(){
+  errorModal.classList.add('hidden')
+}
+
+function findLikes(){
+  const likeArr = document.querySelectorAll('.like-glyph')
+
+  likeArr.forEach((singularLike) => {
+    singularLike.addEventListener('click', () => console.log('YOU FOUND ME!') )
+  })
+}
+
+function clickListener(){
+  document.addEventListener('click', (event) => {
+    if(event.target.classList[0] === 'like-glyph'){
+      mimicServerCall()
+      .then(resp => {
+       const activated = event.target.classList.contains('activated-heart');
+        activated 
+        ? event.target.classList.remove('activated-heart')
+        : event.target.classList.add('activated-heart');
+      })
+      .catch(error => {
+        errorModal.classList.remove('hidden')
+        setTimeout(() => {
+          hideError();
+      }, 3000)
+
+      })
+    }
+  })
+}
 
 
 
